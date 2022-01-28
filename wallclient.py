f22eb456ff4ea22e6f0c1a86d3df1a1a5e6fea59
Kenny-Miller <kennymilarhol@gmail.com>
2022-01-27 20:53:16 -0700

    updated

--- wallclient.py
+++ wallclient.py
@@ -1,26 +1,35 @@
 import socket
-import logging
+from logger import create_logger
+from pynput.mouse import Button, Controller
 
-logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
+class WallClient():
+  def __init__(self,ip,port) -> None:
+    self.logger = create_logger(__name__)
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    self.sock.bind((ip,port))
+    self.mouse = Controller()
+    self.running = False
+  
+  def start(self) -> None:
+    self.logger.debug('Starting Wall Client')
+    self.running = True
+    while self.running:
+      (data, addr) = self.sock.recvfrom(1024)
+      msg = data.decode('utf-8')
+      self.logger.debug(f"[Message]: {msg} from {addr}")
+      match msg:
+        case 'exit': self.running = False
+        case _: self.handle(msg)
+  
+  def handle(self, msg) -> None:
+    (x, y, pressed) = msg.split(" ")
+    self.mouse.position = (x, y)
+    self.mouse.press(Button.left) if pressed == False else self.mouse.release(Button.left)
 
-hostname = socket.gethostname()
-listener_ip = socket.gethostbyname(hostname)
-listener_port = 7200
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((listener_ip, listener_port))
-
-running = True
-
-def handle(msg):
-  # do click stuff here
-  pass
-
-while running:
-  (data, addr) = sock.recvfrom(1024)
-  msg = data.decode('utf-8')
-  logging.debug(f"[Message]: {msg} from {addr}")
-  match msg:
-    case 'exit': running = False
-    case _: handle(msg)
-
+if __name__ == "__main__":
+  hostname = socket.gethostname()
+  listener_ip = socket.gethostbyname(hostname)
+  listener_port = 7200
+  
+  client = WallClient(listener_ip, listener_port)
+  client.start()
